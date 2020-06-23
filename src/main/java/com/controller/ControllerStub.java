218714f82d4b5167f954bf5583b2a383c83611a6
Jorge Cravid <estgv15232@alunos.estgv.ipv.pt>
2020-06-23 14:07:08 +0100

    Implemented method createUser on ControllerStub and ControllerAPI and correspondent tests

--- src/main/java/com/controller/ControllerStub.java
+++ src/main/java/com/controller/ControllerStub.java
@@ -4,16 +4,15 @@ import com.response.CreateUser;
 import okhttp3.MediaType;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
-import java.io.IOException;
-
 
 public class ControllerStub implements ControllerInterface {
-    public Response<CreateUser> createUser(String name, String job) throws IOException {
-        if (name.isBlank() || job.isBlank()) {
+    private final Integer maxLength = 50;
+
+    public Response<CreateUser> createUser(String name, String job) {
+        if (name.isBlank() || job.isBlank() || name.length() > maxLength || job.length() > maxLength) {
             return Response.error(422, ResponseBody.create(MediaType.parse("application/json"), "Invalid Arguments"));
         }
-        Response<CreateUser> response = Response.success(201, new CreateUser("14", "morpheus", "leader", "2020-06-23T03:22:48.471Z"));
-        return response;
+        return Response.success(201, new CreateUser("14", "morpheus", "leader", "2020-06-23T03:22:48.471Z"));
     }
 
     public void getUserById(Integer id) {
--- src/main/java/com/controller/api/ControllerAPI.java
+++ src/main/java/com/controller/api/ControllerAPI.java
@@ -7,18 +7,20 @@ import okhttp3.MediaType;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
+
 import java.io.IOException;
 
 public class ControllerAPI implements ControllerInterface {
+    private final Integer maxLength = 50;
+
     public Response<CreateUser> createUser(String name, String job) throws IOException {
-        if (name.isBlank() || job.isBlank()) {
+        if (name.isBlank() || job.isBlank() || name.length() > maxLength || job.length() > maxLength) {
             return Response.error(422, ResponseBody.create(MediaType.parse("application/json"), "Invalid Arguments"));
         }
         Endpoint endpoint = APIClient.getClient().create(Endpoint.class);
         UserJob userJob = new UserJob("morpheus", "leader");
         Call<CreateUser> request = endpoint.create(userJob.toJsonObject());
-        Response<CreateUser> response = request.execute();
-        return response;
+        return request.execute();
     }
 
     public void getUserById(Integer id) {
